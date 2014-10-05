144edb4933fd5b0a101e68b4f1f3a31bbaa6fecd
Steven L <lam.steven94@yahoo.com>
2014-10-05 01:29:38 -0700

    Removed checkbox dependecy from app

--- js/alternative.js
+++ js/alternative.js
@@ -67,7 +67,9 @@ function GetGoogleData(type, callback) {
   directionsDisplay.setMap(map);
 
   // Decide type of transportation
-  var googleTransType = null
+  //var googleTransType = null
+
+  /*
   switch(type)
   {
     case "walking":
@@ -85,80 +87,86 @@ function GetGoogleData(type, callback) {
     default:
       break;
   }
+  */
 
-  // valid google map type
-  if(googleTransType != null)
-  {
-    var request = {
-    origin: startlocation, 
-    destination: endlocation,
-    travelMode: googleTransType, 
-    transitOptions: {departureTime: new Date()}
-    };
+  var request = {
+  origin: startlocation, 
+  destination: endlocation,
+  travelMode: type, 
+  transitOptions: {departureTime: new Date()}
+  };
 
 
-    directionsService.route(request, function(response, status) {
-      if (status == google.maps.DirectionsStatus.OK) {
-        directionsDisplay.setDirections(response);
-        
-        // calc total time by finding time from each leg
-        var route = directionsDisplay.getDirections()
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+      
+      // calc total time by finding time from each leg
+      var route = directionsDisplay.getDirections()
 
-        // be sure there is at least one route
-        if(route.routes[0] != null)
+      // be sure there is at least one route
+      if(route.routes[0] != null)
+      {
+        // array of all the legs on this route
+        var legs = route.routes[0].legs
+
+        // Traffic time or no?
+        var traffic = false
+
+        // Total time in seconds
+        var totalTime = 0
+
+        // Total distance in meters
+        var totalDistance = 0
+
+        // go through all legs and total up the time
+        for(var i = 0; i < legs.length; i++)
         {
-          // array of all the legs on this route
-          var legs = route.routes[0].legs
+          if(traffic)
+            totalTime = legs[i].duration_in_traffic.value
+          else
+            totalTime = legs[i].duration.value
 
-          // Traffic time or no?
-          var traffic = false
-
-          // Total time in seconds
-          var totalTime = 0
-
-          // Total distance in meters
-          var totalDistance = 0
-
-          // go through all legs and total up the time
-          for(var i = 0; i < legs.length; i++)
-          {
-            if(traffic)
-              totalTime = legs[i].duration_in_traffic.value
-            else
-              totalTime = legs[i].duration.value
-
-            // Add distance of leg to total distance
-            totalDistance += legs[i].distance.value
-          }
-
-          var hrs = Math.floor(totalTime / 3600)
-          totalTime = totalTime % 3600
-          var mins = Math.floor(totalTime / 60)
-          totalTime = totalTime % 60
-          var secs = totalTime
-
-          // Call the callback function when query is done
-          callback({
-            'Type' : type,
-            'Hours' : hrs, 
-            'Minutes' : mins,
-            'Seconds' : secs,
-            // convert meters -> miles and round 2 decimal values
-            // to fixed returns a string and not a number
-            'Distance' : ((totalDistance * 0.000621371).toFixed(2)/1)
-          })
+          // Add distance of leg to total distance
+          totalDistance += legs[i].distance.value
         }
+
+        var hrs = Math.floor(totalTime / 3600)
+        totalTime = totalTime % 3600
+        var mins = Math.floor(totalTime / 60)
+        totalTime = totalTime % 60
+        var secs = totalTime
+
+        // Call the callback function when query is done
+        callback({
+          'Type' : type,
+          'Hours' : hrs, 
+          'Minutes' : mins,
+          'Seconds' : secs,
+          // convert meters -> miles and round 2 decimal values
+          // to fixed returns a string and not a number
+          'Distance' : ((totalDistance * 0.000621371).toFixed(2)/1)
+        })
       }
-    });
-  }
+    }
+    else
+    {
+      callback({
+          'Type' : type,
+          'Hours' : null, 
+          'Minutes' : null,
+          'Seconds' : null,
+          'Distance' : null
+      })
+    }
+  });
 }
 
 function GetDuration()
 {
-
   var durationData = []
-
-  var inputElements = document.getElementsByClassName('transportation');
+  /*
+  var inputElements = document.getElementsByClassName('transportation')
   for(var i=0; inputElements[i]; ++i){
         if(inputElements[i].checked){
 
@@ -186,6 +194,25 @@ function GetDuration()
           })
         }
   }
+  */
+
+  // Array to store all google transportation types
+  var transportationType = [google.maps.TravelMode.WALKING, 
+    google.maps.TravelMode.DRIVING, google.maps.TravelMode.BICYCLING, 
+    google.maps.TravelMode.TRANSIT]
+
+  for(var i = 0; i < transportationType.length; i++)
+    GetGoogleData(transportationType[i], function(data) {
+              durationData.push(data); 
+              if(durationData.length == transportationType.length)
+              {
+                // When all data is done, print it
+                alertUser(durationData)
+                var jsonFile = make_json(durationData)
+                console.log(jsonFile)
+              }
+          })
+
 }
 
 // print out all the data
@@ -212,6 +239,18 @@ function make_json(data) {
           final[data[i].Type] = data[i];
   }
   return final;
+}
+
+// Caculat driving cost ; time_drive is minutes
+function driving_cost(distance, time_drive) {
+  var cost = (distance / 25) * 3.80;
+  if (time_drive > 25) {
+    cost += time_drive * 0.05
+  }
+  else {
+    cost += time_drive * 0.1
+  }
+  return cost
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
